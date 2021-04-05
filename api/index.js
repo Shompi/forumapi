31110efed2cb6d6495e8c7c749cf098df5229c8a
ShompiFlen <shompire02@gmail.com>
2021-04-04 20:49:06 -0400

    added some files

---
+++ api/index.js
@@ -0,0 +1,17 @@
+const { Router } = require('express');
+const router = Router();
+
+// Ruta base de la API, administra todos los métodos http (GET, POST, UPDATE, PATCH, etc)
+router.all('/', (req, res, next) => {
+
+	// Si el request acepta un JSON devuelta
+	if (req.headers.accept == "application/json") {
+		res.send({ message: "Hello!", status: 200 });
+
+	} else {
+		// Si no mandamos una respuesta solo con texto.
+		res.send("Hello there!");
+	}
+});
+
+module.exports = router;
--- app.js
+++ app.js
@@ -16,33 +16,37 @@ app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(express.json());
 
-
-
-
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', require('./routes/index'));
-app.use('/muki', require('./routes/muki'));
-app.use('/exiliados', require('./routes/exiliados'));
+app.use('/muki', require('./api/muki'));
+app.use('/exiliados', require('./api/exiliados'));
 
-// catch 404 and forward to error handler
-app.use((req, res, next) => {
-  next(createError(404));
+app.get('/home*', (req, res) => {
+	res.redirect("home.html");
 });
+
+app.get('/mayero', (req, res, next) => {
+	res.redirect("html5_final.html");
+});
+
+app.get('*', (req, res) => {
+	res.redirect("notfound.html");
+})
+
 
 // error handler en caso de que falle el parseado a json (entity.parse.failed)
 app.use((err, req, res, next) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  if (err.type === "entity.parse.failed") {
-    console.log("Se recibió un request con un json que no pudo ser parseado.");
-    console.log(req.body);
-    res.send({ message: "Ocurrió un error al parsear el JSON.", status: 400 });
-  }
+	if (err.type === "entity.parse.failed") {
+		console.log("Se recibió un request con un json que no pudo ser parseado.");
+		console.log(req.body);
+		res.send({ message: "Ocurrió un error al parsear el JSON.", status: 400 });
+	}
 });
 
 module.exports = app;
